0577f5000d008fbe38a1a76b0cd0c5cbd023e8fc
ganeshris <67224230+ganeshris@users.noreply.github.com>
2022-11-02 08:47:22 +0530

    CHANGES FOR TEST3

--- dockermt/src/main/resources/ScriptFiles/copy.sh
+++ dockermt/src/main/resources/ScriptFiles/copy.sh
@@ -6,6 +6,6 @@ PRJ_NAME=cns-portal-backend
 GIT_USER=farhan23432
 GIT_PASS=Farhan@23432
 GIT_URL_FROM=http://13.235.248.157:3000/admin123/test1.git
-GIT_URL_TO=http://13.235.248.157:3000/admin123/test2.git
+GIT_URL_TO=http://13.235.248.157:3000/admin123/test3.git
 docker build .
 echo IMAGE_NAME=$GIT_URL_TO
