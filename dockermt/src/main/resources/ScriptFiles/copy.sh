91f01d15775e6225593b3aecfce64eb775ea000f
latikeshris <108168583+latikeshris@users.noreply.github.com>
2022-11-02 15:47:54 +0530

    Update copy.sh

--- dockermt/src/main/resources/ScriptFiles/copy.sh
+++ dockermt/src/main/resources/ScriptFiles/copy.sh
@@ -5,7 +5,7 @@ Below is the script to copy reporsitry
 PRJ_NAME=cns-portal-backend
 GIT_USER=farhan23432
 GIT_PASS=Farhan@23432
-GIT_URL_FROM=http://13.235.248.157:3000/admin123/test1.git
-GIT_URL_TO=http://13.235.248.157:3000/admin123/test3.git
+GIT_URL_FROM=http://13.126.217.36:31633/admin123/test1.git
+GIT_URL_TO=http://13.126.217.36:31633/admin123/test3.git
 docker build .
 echo IMAGE_NAME=$GIT_URL_TO
